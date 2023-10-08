dd3dc7da241643df7a25495adfe81119d7bdd63c
Devbijjwe <22devendrabijwe@gmail.com>
2023-10-08 20:37:09 +0530

    Updated

--- views.py
+++ views.py
@@ -25,6 +25,7 @@ with open('config.json',"r") as c:
 @app.route("/",methods=["GET","POST","PUT","DELETE","HEAD"])
 def index():
     song_id = Songs.get_random_song_id()
+    song_id="9047c1b666"
     
     return redirect(url_for('playsong', song_id=song_id))
     
